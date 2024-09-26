ef4448a04e23aac0f9502f770624f71b8f996e13
2189217048 <2189217048@qq.com>
2024-09-26 20:54:08 +0800

    修复bug

--- 自动选课.py
+++ 自动选课.py
@@ -2,18 +2,11 @@ import cqrk
 
 stdID = '学号'
 
-# 使用webvpn登录，校外登录使用
-webVpn = cqrk.webvpn()
-webVpn.username = stdID
-webVpn.password = 'webvpn密码'
-webVpn.auth()
-
-# 使用webvpn登录时，use_web_vpn参数必须为True
-User = cqrk.user(use_web_vpn=True)
+User = cqrk.user()
 User.username = stdID
-User.password = '教务系统密码'
+User.password = '教务系统登录密码'
 User.auth()
-
-Jwxt = cqrk.jwxt(User.loadCookie(),use_web_vpn=True)
+cookies = User.loadCookie()
+Jwxt = cqrk.jwxt(cookies,use_web_vpn=False)
 
 print(Jwxt.joinCourse('乒乓球','老师名称','体育'))
